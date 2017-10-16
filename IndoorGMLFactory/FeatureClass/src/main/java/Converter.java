00b93d5e17616f304cea0f6098a599638f8be0e1
JungHam <wjdgpal7@gmail.com>
2017-10-16 17:08:17 +0900

    create converter for FeatureClass <-> JAXB

---
+++ IndoorGMLFactory/FeatureClass/src/main/java/Converter.java
@@ -0,0 +1,95 @@
+import net.opengis.gml.v_3_2_1.AbstractFeatureType;
+import net.opengis.indoorgml.core.v_1_0.CellSpaceBoundaryType;
+import net.opengis.indoorgml.core.v_1_0.CellSpaceType;
+import net.opengis.indoorgml.core.v_1_0.EdgesType;
+import net.opengis.indoorgml.core.v_1_0.ExternalObjectReferenceType;
+import net.opengis.indoorgml.core.v_1_0.ExternalReferenceType;
+import net.opengis.indoorgml.core.v_1_0.IndoorFeaturesType;
+import net.opengis.indoorgml.core.v_1_0.InterEdgesType;
+import net.opengis.indoorgml.core.v_1_0.InterLayerConnectionType;
+import net.opengis.indoorgml.core.v_1_0.MultiLayeredGraphType;
+import net.opengis.indoorgml.core.v_1_0.NodesType;
+import net.opengis.indoorgml.core.v_1_0.PrimalSpaceFeaturesType;
+import net.opengis.indoorgml.core.v_1_0.SpaceLayerClassTypeType;
+import net.opengis.indoorgml.core.v_1_0.SpaceLayerType;
+import net.opengis.indoorgml.core.v_1_0.StateType;
+import net.opengis.indoorgml.core.v_1_0.TransitionType;
+import net.opengis.indoorgml.core.v_1_0.TypeOfTopoExpressionCodeEnumerationType;
+
+public class Converter {
+	AbstractFeatures change2FeatureClass(AbstractFeatureType feature) {
+		return null;
+	}
+
+	CellSpace change2FeatureClass(CellSpaceType feature) {
+		return null;
+	}
+
+	CellSpaceBoundary change2FeatureClass(CellSpaceBoundaryType feature) {
+		return null;
+	}
+
+	/*
+	 * 
+	 * CellSpaceBoundaryGeometry
+	 * change2FeatureClass(CellSpaceBoundaryGeometryType feature) { return null;
+	 * } CellSpaceGeometry change2FeatureClass(CellSpaceGeometryType feature) {
+	 * return null; }
+	 */
+
+	Edges change2FeatureClass(EdgesType feature) {
+		return null;
+	}
+
+	ExternalObjectReference change2FeatureClass(ExternalObjectReferenceType feature) {
+		return null;
+	}
+
+	ExternalReference change2FeatureClass(ExternalReferenceType feature) {
+		return null;
+	}
+
+	IndoorFeatures change2FeatureClass(IndoorFeaturesType feature) {
+		return null;
+	}
+
+	InterEdges change2FeatureClass(InterEdgesType feature) {
+		return null;
+	}
+
+	InterLayerConnection change2FeatureClass(InterLayerConnectionType feature) {
+		return null;
+	}
+
+	MultiLayeredGraph change2FeatureClass(MultiLayeredGraphType feature) {
+		return null;
+	}
+
+	Nodes change2FeatureClass(NodesType feature) {
+		return null;
+	}
+
+	PrimalSpaceFeatures change2FeatureClass(PrimalSpaceFeaturesType feature) {
+		return null;
+	}
+
+	SpaceLayer change2FeatureClass(SpaceLayerType feature) {
+		return null;
+	}
+
+	SpaceLayerClassType change2FeatureClass(SpaceLayerClassTypeType feature) {
+		return null;
+	}
+
+	State change2FeatureClass(StateType feature) {
+		return null;
+	}
+
+	Transition change2FeatureClass(TransitionType feature) {
+		return null;
+	}
+
+	typeOfTopoExpressionCode change2FeatureClass(TypeOfTopoExpressionCodeEnumerationType feature) {
+		return null;
+	}
+}
