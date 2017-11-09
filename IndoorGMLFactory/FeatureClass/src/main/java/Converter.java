c0d437bf2ccf0add85e4daf60bbbc2e36f870d27
JungHam <wjdgpal7@gmail.com>
2017-11-10 01:46:54 +0900

    create function for changing FeatureClass instance to JAXB Class

--- IndoorGMLFactory/FeatureClass/src/main/java/Converter.java
+++ IndoorGMLFactory/FeatureClass/src/main/java/Converter.java
@@ -5,7 +5,9 @@ import net.opengis.gml.v_3_2_1.AbstractFeatureType;
 import net.opengis.gml.v_3_2_1.CompositeSolidType;
 import net.opengis.gml.v_3_2_1.CompositeSurfaceType;
 import net.opengis.gml.v_3_2_1.CurveType;
+import net.opengis.gml.v_3_2_1.SolidPropertyType;
 import net.opengis.gml.v_3_2_1.SolidType;
+import net.opengis.gml.v_3_2_1.SurfacePropertyType;
 import net.opengis.gml.v_3_2_1.SurfaceType;
 import net.opengis.indoorgml.core.v_1_0.CellSpaceBoundaryType;
 import net.opengis.indoorgml.core.v_1_0.CellSpacePropertyType;
@@ -90,7 +92,32 @@ public class Converter {
 		
 		return newFeature;
 	}
+	public CellSpaceType change2JaxbClass(CellSpace feature) {
+		//JAXBContextImpl jc = (JAXBContextImpl) JAXBContextImpl.newInstance(CellSpaceType.class);
+		CellSpaceType newFeature = new CellSpaceType();
+		StatePropertyType duality = new StatePropertyType();
+		StateType referredState = new StateType();
 
+		referredState.setId(feature.duality);
+		duality.setState(referredState);
+
+		newFeature.setDuality(duality);
+		newFeature.setId(feature.ID);
+		newFeature.setPartialboundedBy(feature.partialboundedBy);
+		
+		if(feature.geometryType == "SurfaceType"){
+			newFeature.setGeometry2D((SurfacePropertyType)feature.cellSpaceGeometryObject);
+		}
+		else if(feature.geometryType == "CompositeSurfaceType"){}
+		else if(feature.geometryType == "SolidType"){
+			newFeature.setGeometry3D((SolidPropertyType)feature.cellSpaceGeometryObject);
+		}
+		else if(feature.geometryType == "CompositeSolidType"){}
+		
+		return newFeature;
+	}
+	
+	
 	CellSpaceBoundary change2FeatureClass(CellSpaceBoundaryType feature) {
 		CellSpaceBoundary newFeature = new CellSpaceBoundary();
 		
