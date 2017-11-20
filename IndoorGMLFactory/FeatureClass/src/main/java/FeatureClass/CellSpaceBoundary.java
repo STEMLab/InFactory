e25da4fdc773ac46ea75d1502e5a2ff50a6c70ca
JungHam <wjdgpal7@gmail.com>
2017-11-21 01:24:39 +0900

    create get,set function of FeatureClass.CellSpaceBoundary

--- IndoorGMLFactory/FeatureClass/src/main/java/FeatureClass/CellSpaceBoundary.java
+++ IndoorGMLFactory/FeatureClass/src/main/java/FeatureClass/CellSpaceBoundary.java
@@ -1,4 +1,5 @@
 package FeatureClass;
+
 /**
  * @author jungh
  *	Implements CellSpaceBoundaryType of IndoorGML 1.0.3
@@ -24,5 +25,20 @@ public class CellSpaceBoundary {
 	 * If External Reference of the feature is exist, then set this.
 	 */
 	public ExternalReference externalReference;
+	
+	public String getID(){ return this.ID; }
+	public void setID(String id){ this.ID = id;} 
+	
+	public Object getCellSpaceBoundaryGeometry(){return this.cellSpaceBoundaryGeometry;}
+	public void setCellSpaceGeometry(CellSpaceGeometry csg){ this.cellSpaceBoundaryGeometry = csg; }
+	
+	public String getDuality(){return this.duality;}
+	public void setDuality(String d){this.duality = d;}
+
+	public String getName(){return this.name;}
+	public void setName(String name){this.name = name;}
+	
+	public ExternalReference getExternalReference(){return this.externalReference;}
+	public void setExternalReference(ExternalReference e){this.externalReference = e;}
 
 }
