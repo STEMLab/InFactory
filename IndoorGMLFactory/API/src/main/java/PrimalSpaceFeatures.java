f5c9d321ed39015506d4efe0bfbfe2435b42931f
JungHam <wjdgpal7@gmail.com>
2017-12-18 03:31:30 +0900

    edit updatePrimalSpaceFeatures and deletePrimalSpaceFeatures

--- IndoorGMLFactory/API/src/main/java/PrimalSpaceFeatures.java
+++ IndoorGMLFactory/API/src/main/java/PrimalSpaceFeatures.java
@@ -2,6 +2,7 @@
 
 import java.util.List;
 
+import Binder.IndoorGMLMap;
 import Binder.docData;
 import FeatureClass.CellSpace;
 import FeatureClass.CellSpaceBoundary;
@@ -60,14 +61,67 @@ public class PrimalSpaceFeatures {
 	public FeatureClass.PrimalSpaceFeatures updatePrimalSpaceFeatures(String ID, List<CellSpace> csl, List<CellSpaceBoundary> csbl) {
 		return null;
 	}
+	
+	public FeatureClassReference.PrimalSpaceFeatures updatePrimalSpaceFeatures(String docId, String Id, String attributeType,
+			String updateType, List<String>object ) {
+		FeatureClassReference.PrimalSpaceFeatures target = null;
+		if (docData.docs.hasFeature(docId, Id)) {
+			target = (FeatureClassReference.PrimalSpaceFeatures) docData.docs.getFeature(docId, Id);
+			if (attributeType.equals("cellSpaceMember")) {
+				List<String>cellSpaceMember = target.getCellSpaceMember();
+				if(updateType.equals("add")){
+					cellSpaceMember.addAll(object);
+				}
+				//TODO : add cellSpace to cellSpace container and ID container
+				else if(updateType.equals("delete")){
+					for(int i = 0 ; i < object.size();i++){
+						if(cellSpaceMember.contains(object.get(i))){
+							cellSpaceMember.remove(object.get(i));
+						}
+					}
+				//TODO : remove cellSpace at cellSpace container and ID container?
+				}
+				
+			} else if (attributeType.equals("cellSpaceBoundaryMember")) {
+				List<String>cellSpaceBoundaryMember = target.getCellSpaceBoundaryMember();
+				if(updateType.equals("add")){
+					cellSpaceBoundaryMember.addAll(object);
+				}
+				//TODO : add cellSpace to cellSpace container and ID container
+				else if(updateType.equals("delete")){
+					for(int i = 0 ; i < object.size();i++){
+						if(cellSpaceBoundaryMember.contains(object.get(i))){
+							cellSpaceBoundaryMember.remove(object.get(i));
+						}
+					}
+				//TODO : remove cellSpace at cellSpace container and ID container?
+					//answer : because relationship is aggregation, so do not have.
+				}
 
+			}  else {
+				System.out.println("update error in cellSpaceType : there is no such attribute name");
+			}
+		} else {
+			System.out.println("there is no name with Id :" + Id + " in document Id : " + docId);
+		}
+		return target;
+	}
 	/**
 	 * Search PrimalSpaceFeatures feature and delete it
 	 * @param ID ID of target
 	 * 
 	 */
-	public void deletePrimalSpaceFeatures(String ID) {
-		
+	public void deletePrimalSpaceFeatures(String docId, String Id) {
+		if (docData.docs.hasFeature(docId, Id)) {
+			IndoorGMLMap doc = docData.docs.getDocument(docId);
+			FeatureClassReference.PrimalSpaceFeatures target = (FeatureClassReference.PrimalSpaceFeatures) docData.docs.getFeature(docId,
+					Id);
+			// String duality = target.getd;
+			doc.getFeatureContainer("PrimalSpaceFeatures").remove(Id);
+			doc.getFeatureContainer("ID").remove(Id);
+			
+			
+		}
 	}
 
 }
