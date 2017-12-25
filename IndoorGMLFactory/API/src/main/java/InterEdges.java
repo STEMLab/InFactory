1d0744ae430c3559ae03be2e091f268797279e3c
JungHam <wjdgpal7@gmail.com>
2017-12-25 16:42:35 +0900

    edit updateInterEdges

--- IndoorGMLFactory/API/src/main/java/InterEdges.java
+++ IndoorGMLFactory/API/src/main/java/InterEdges.java
@@ -24,6 +24,37 @@ public class InterEdges {
 		FeatureClass.InterEdges target = null;
 		return target;
 	}
-	public static FeatureClassReference.InterEdges updateInterEdges(String docId, String Id, String attributeType, String updateType, Object o){}
+	public static FeatureClassReference.InterEdges updateInterEdges(String docId, String Id, String attributeType, String updateType, Object o){
+		FeatureClassReference.InterEdges target = null;
+		if (docData.docs.hasFeature(docId, Id)) {
+			target = (FeatureClassReference.InterEdges)docData.docs.getFeature(docId, Id);
+			if(attributeType.equals("interLayerConnectionMember")){
+				List<String>interLayerConnectionMember = target.getInterLayerConnectionMember();
+				if(updateType != null){
+					List<String>object = (List<String>)o;	
+					if(updateType.equals("add")){
+						interLayerConnectionMember.addAll((List<String>)object);
+					}
+					else if(updateType.equals("remove")){
+						for(int i = 0 ; i < object.size();i++){
+							if(interLayerConnectionMember.contains(object.get(i))){
+								interLayerConnectionMember.remove(object.get(i));
+								InterLayerConnection.deleteInterLayerConnection(docId, Id);
+							}
+							
+						}
+					}
+				}
+				if(interLayerConnectionMember.size() != 0){
+					target.setInterLayerConnectionMember(interLayerConnectionMember);
+				}
+				else
+					System.out.println("Error at updateInterEdges : empty stateMember cannot be submited");
+				
+			}
+			docData.setFeature(docId, Id, "InterEdges", target);
+		}
+		return target;
+	}
 	public static void deleteInterEdges(String docId, String Id){}
 }
