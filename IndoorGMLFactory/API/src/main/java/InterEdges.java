ab6831624e67b7dddeb8c8685c6bd85adeedf620
JungHam <wjdgpal7@gmail.com>
2017-12-25 16:37:29 +0900

    edit createInterEdges

--- IndoorGMLFactory/API/src/main/java/InterEdges.java
+++ IndoorGMLFactory/API/src/main/java/InterEdges.java
@@ -1,7 +1,25 @@
 import java.util.List;
 
+import Binder.docData;
+
 public class InterEdges {
-	public static FeatureClassReference.InterEdges createInterEdges(String docId, String parentId, String Id, List<String>interLayerConnectionMember){}
+	public static FeatureClassReference.InterEdges createInterEdges(String docId, String parentId, String Id, List<String>interLayerConnectionMember){
+		FeatureClassReference.InterEdges newFeature = null;
+		if (docData.docs.hasDoc(docId)) {
+			newFeature.setID(Id);
+			newFeature.setParentID(parentId);
+			if(interLayerConnectionMember != null){
+				newFeature.setInterLayerConnectionMember(interLayerConnectionMember);
+			}
+			else{
+				System.out.println("Error at createInterEdges : there is no InterLayerConnectionMember");
+			}
+			
+			docData.docs.setFeature(docId, Id, "InterEdges", newFeature);
+		}
+		
+		return newFeature;
+	}
 	public static FeatureClass.InterEdges readInterEdges(String docId, String Id){}
 	public static FeatureClassReference.InterEdges updateInterEdges(String docId, String Id, String attributeType, String updateType, Object o){}
 	public static void deleteInterEdges(String docId, String Id){}
