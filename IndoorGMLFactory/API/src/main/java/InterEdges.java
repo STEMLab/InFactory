14c018bad3f618cd0db9edbc5157aaee42fad214
JungHam <wjdgpal7@gmail.com>
2017-12-25 16:35:46 +0900

    create CRUD functino at API.InterEdges

--- IndoorGMLFactory/API/src/main/java/InterEdges.java
+++ IndoorGMLFactory/API/src/main/java/InterEdges.java
@@ -1,4 +1,8 @@
+import java.util.List;
 
 public class InterEdges {
-	
+	public static FeatureClassReference.InterEdges createInterEdges(String docId, String parentId, String Id, List<String>interLayerConnectionMember){}
+	public static FeatureClass.InterEdges readInterEdges(String docId, String Id){}
+	public static FeatureClassReference.InterEdges updateInterEdges(String docId, String Id, String attributeType, String updateType, Object o){}
+	public static void deleteInterEdges(String docId, String Id){}
 }
