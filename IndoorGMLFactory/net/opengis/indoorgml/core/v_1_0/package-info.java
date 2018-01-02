254a33812afad9885fdf6616b8f44a6a19fa90c7
JungHam <wjdgpal7@gmail.com>
2018-01-02 15:39:57 +0900

    remove docx and classes not used

--- IndoorGMLFactory/net/opengis/indoorgml/core/v_1_0/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.2.11 버전을 통해 생성되었습니다. 
-// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>를 참조하십시오. 
-// 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
-// 생성 날짜: 2017.11.22 시간 01:17:09 AM KST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.opengis.net/indoorgml/1.0/core", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.opengis.indoorgml.core.v_1_0;
