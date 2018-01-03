090d06cdd3f99504e1ea325f77e0b59c6499f630
JungHam <wjdgpal7@gmail.com>
2018-01-03 15:41:10 +0900

    edit feature.CellSpaceBoundary for extending AbstractFeature

--- FeatureClass/src/main/java/edu/pnu/stem/feature/CellSpaceBoundary.java
+++ FeatureClass/src/main/java/edu/pnu/stem/feature/CellSpaceBoundary.java
@@ -1,64 +1,71 @@
 package edu.pnu.stem.feature;
 
 /**
- * @author jungh
- *	Implements CellSpaceBoundaryType of IndoorGML 1.0.3
+ * @author jungh Implements CellSpaceBoundaryType of IndoorGML 1.0.3
  */
-public class CellSpaceBoundary {
+public class CellSpaceBoundary extends AbstractFeature {
+
 	/**
-	 * ID of this feature
-	 */
-	public String ID;
-	/**
-	 * Name of this feature
-	 */
-	public String name;
-	/**
-	 * value of Transition which has duality relationship with this CellSpaceBoundary
+	 * value of Transition which has duality relationship with this
+	 * CellSpaceBoundary
 	 */
 	public String duality;
-
 
 	/**
 	 * If External Reference of the feature is exist, then set this.
 	 */
 	public String externalReference;
-	
+
 	/**
 	 * ID of parent feature instance
 	 */
 	public String parentID;
 	public String cellSpaceBoundaryGeometry;
-	
-	public String getID(){ return this.ID; }
-	public void setID(String id){ this.ID = id;} 
-	public void setParentID(String id){this.parentID = id;}
-	public String getParentID(){return this.parentID;}
-	public Object getParentInstance(){
+
+	public void setParentID(String id) {
+		this.parentID = id;
+	}
+
+	public String getParentID() {
+		return new String(this.parentID);
+	}
+
+	public Object getParentInstance() {
 		Object feature = null;
 		feature = IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("PrimalSpaceFeatures"), this.parentID);
 		return feature;
 	}
-	
-	
 
-	public String getDuality(){return this.duality;}
-	public Object getDualityInstance(){return IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("Transition"), this.duality);}
-	public void setDuality(String d){this.duality = d;}
+	public String getDuality() {
+		return new String(this.duality);
+	}
 
-	public String getName(){return this.name;}
-	public void setName(String name){this.name = name;}
-	
-	public String getExternalReference(){return this.externalReference;}
-	public void setExternalReference(String e){this.externalReference = e;}
+	public Object getDualityInstance() {
+		return IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("Transition"), this.duality);
+	}
+
+	public void setDuality(String d) {
+		this.duality = d;
+	}
+
+	public String getExternalReference() {
+		return new String(this.externalReference);
+	}
+
+	public void setExternalReference(String e) {
+		this.externalReference = e;
+	}
+
 	/**
 	 * @return the cellSpaceBoundaryGeometry
 	 */
 	public String getCellSpaceBoundaryGeometry() {
-		return cellSpaceBoundaryGeometry;
+		return new String(cellSpaceBoundaryGeometry);
 	}
+
 	/**
-	 * @param cellSpaceBoundaryGeometry the cellSpaceBoundaryGeometry to set
+	 * @param cellSpaceBoundaryGeometry
+	 *            the cellSpaceBoundaryGeometry to set
 	 */
 	public void setCellSpaceBoundaryGeometry(String cellSpaceBoundaryGeometry) {
 		this.cellSpaceBoundaryGeometry = cellSpaceBoundaryGeometry;
