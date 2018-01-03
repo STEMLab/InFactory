cf88a78467a2e3132dcc491cb88131d4779fffc9
JungHam <wjdgpal7@gmail.com>
2018-01-03 15:50:58 +0900

    edit feature.InterLayerConnection for extends AbstractFeature

--- FeatureClass/src/main/java/edu/pnu/stem/feature/InterLayerConnection.java
+++ FeatureClass/src/main/java/edu/pnu/stem/feature/InterLayerConnection.java
@@ -1,88 +1,75 @@
 package edu.pnu.stem.feature;
+
 /**
- * @author jungh
- *	Implements InteraLayerConnectionType of IndoorGML 1.0.3
+ * @author jungh Implements InteraLayerConnectionType of IndoorGML 1.0.3
  */
-public class InterLayerConnection {
-	/**
-	 * ID of this feature
-	 */
-	public String ID;
-	/**
-	 * Name of this feature
-	 */
-	public String name;
-	/**
-	 * save the topology type value
-	 */
+public class InterLayerConnection extends AbstractFeature {
+	
+	public String parentID;
+	
 	public typeOfTopoExpressionCode typeOfTopoExpression;
 	/**
 	 * describe characteristic of this instance
 	 */
 	public String comment;
 	/**
-	 * save list of ID of States which are related with each others as this InterLayerConnection
+	 * save list of ID of States which are related with each others as this
+	 * InterLayerConnection
 	 */
 	public String[] interConnects;
 	/**
-	 * save list of ID of SpaceLayers which are related with each others as this InterLayerConnection
+	 * save list of ID of SpaceLayers which are related with each others as this
+	 * InterLayerConnection
 	 */
 	public String[] connectedLayers;
-	
-	public Boolean checkInterConnectsNumber(){
-		//Boolean flag = false;
-		if(this.interConnects.length != 2 || this.connectedLayers.length != 2){
-			return false;			
+
+	public Boolean checkInterConnectsNumber() {
+		// Boolean flag = false;
+		if (this.interConnects.length != 2 || this.connectedLayers.length != 2) {
+			return false;
 		}
-		return true; 
-					
+		return true;
+
 	}
-	public String parentID;
-	public void setID(String id){this.ID = id;};
-	public String getID(){return this.ID;};
-	public void setParentID(String id){this.parentID = id;}
-	public String getParentID(){return this.parentID;}
 	
-	public void setConnectedLayers(String[] cl){this.connectedLayers = cl;}
-	public String[] getConnectedLayers(){return this.connectedLayers;}
-	
-	public void setInterConnects(String[] ic){this.interConnects = ic;}
-	public String[] getInterConnects(){return this.interConnects;}
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
+	public void setParentID(String id) {
+		this.parentID = id;
 	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
+
+	public String getParentID() {
+		return new String(this.parentID);
 	}
+
+	public void setConnectedLayers(String[] cl) {
+		this.connectedLayers = cl;
+	}
+
+	public String[] getConnectedLayers() {
+		return this.connectedLayers;
+	}
+
+	public void setInterConnects(String[] ic) {
+		this.interConnects = ic;
+	}
+
+	public String[] getInterConnects() {
+		return this.interConnects;
+	}
+
 	/**
 	 * @return the typeOfTopoExpression
 	 */
 	public typeOfTopoExpressionCode getTypeOfTopoExpression() {
 		return typeOfTopoExpression;
 	}
+
 	/**
-	 * @param typeOfTopoExpression the typeOfTopoExpression to set
+	 * @param typeOfTopoExpression
+	 *            the typeOfTopoExpression to set
 	 */
 	public void setTypeOfTopoExpression(typeOfTopoExpressionCode typeOfTopoExpression) {
 		this.typeOfTopoExpression = typeOfTopoExpression;
 	}
-	/**
-	 * @return the comment
-	 */
-	public String getComment() {
-		return comment;
-	}
-	/**
-	 * @param comment the comment to set
-	 */
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+
 
 }
