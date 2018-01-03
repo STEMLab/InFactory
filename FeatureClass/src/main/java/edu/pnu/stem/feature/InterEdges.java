7996def7f39049efd181f7efcdb46a9dc7a20064
JungHam <wjdgpal7@gmail.com>
2018-01-03 15:49:19 +0900

    edit feature.InterEdges for extending AbstractFeature

--- FeatureClass/src/main/java/edu/pnu/stem/feature/InterEdges.java
+++ FeatureClass/src/main/java/edu/pnu/stem/feature/InterEdges.java
@@ -1,69 +1,48 @@
 package edu.pnu.stem.feature;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author jungh
- *	Implements InterEdgesType of IndoorGML 1.0.3
+ * @author jungh Implements InterEdgesType of IndoorGML 1.0.3
  */
-public class InterEdges {
+public class InterEdges extends AbstractFeature {
+
 	/**
-	 * ID of this feature
+	 * hold list of InterLayerConnections
 	 */
-	public String ID;
-	/**
-	 * Name of this feature
-	 */
-	public String name;
-	/**
-	 * hold list of InterLayerConnections 
-	 */
-	public List<String> interLayerConnectionMember; 
-	
+	public List<String> interLayerConnectionMember;
+
 	public String parentID;
-	
-	public void setParetID(String pi){this.parentID = pi;}
-	public String getParentID(){return this.parentID;}
-	/**
-	 * @return the iD
-	 */
-	public String getID() {
-		return ID;
+
+	public void setParetID(String pi) {
+		this.parentID = pi;
 	}
-	/**
-	 * @param iD the iD to set
-	 */
-	public void setID(String iD) {
-		ID = iD;
+
+	public String getParentID() {
+		return new String(this.parentID);
 	}
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	/**
+
+		/**
 	 * @return the interLayerConnectionMember
 	 */
 	public List<String> getInterLayerConnectionMember() {
-		return interLayerConnectionMember;
+		return new ArrayList<String>(interLayerConnectionMember);
 	}
+
 	/**
-	 * @param interLayerConnectionMember the interLayerConnectionMember to set
+	 * @param interLayerConnectionMember
+	 *            the interLayerConnectionMember to set
 	 */
 	public void setInterLayerConnectionMember(List<String> interLayerConnectionMember) {
 		this.interLayerConnectionMember = interLayerConnectionMember;
 	}
+
 	/**
-	 * @param parentID the parentID to set
+	 * @param parentID
+	 *            the parentID to set
 	 */
 	public void setParentID(String parentID) {
 		this.parentID = parentID;
 	}
 }
-
