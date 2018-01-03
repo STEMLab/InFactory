0bccb820362662f24966dca2a1159a9484b99313
JungHam <wjdgpal7@gmail.com>
2018-01-03 15:42:07 +0900

    edit feature.CellSpaceBoundaryGeometry for extending AbstractFeature

--- FeatureClass/src/main/java/edu/pnu/stem/feature/CellSpaceBoundaryGeometry.java
+++ FeatureClass/src/main/java/edu/pnu/stem/feature/CellSpaceBoundaryGeometry.java
@@ -4,27 +4,14 @@ package edu.pnu.stem.feature;
  * @author jungh
  *	Implements CellSpaceBoundaryGeometryType of IndoorGML 1.0.3
  */
-public class CellSpaceBoundaryGeometry {
-	/**
-	 * ID of this feature
-	 */
-	public String ID;
-	/**
-	 * Name of this feature
-	 */
-	public String name;
-	/**
-	 * save geometry of feature 
-	 */	
+public class CellSpaceBoundaryGeometry extends AbstractFeature{
+	
 	public String geometry2D;
 	public String geometry3D;
 	/**
 	 * ID of parent feature instance.
 	 */	
 	public String parentID;
-	
-	public String getID(){ return this.ID; }
-	public void setID(String id){ this.ID = id;} 
 	
 	public String getParentID(){return this.parentID;}
 	public void setParentID(String id){this.parentID = id;}
