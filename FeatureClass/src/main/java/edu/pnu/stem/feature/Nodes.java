178f4d3d532b0eb858c8c2e4700b708b44a5dadc
JungHam <wjdgpal7@gmail.com>
2018-01-04 21:02:06 +0900

    edit getter and setter of Nodes

--- FeatureClass/src/main/java/edu/pnu/stem/feature/Nodes.java
+++ FeatureClass/src/main/java/edu/pnu/stem/feature/Nodes.java
@@ -3,28 +3,55 @@ package edu.pnu.stem.feature;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Nodes extends AbstractFeature{
+public class Nodes extends AbstractFeature {
 	/**
 	 * State list which this feature contains
 	 */
 	List<String> stateMember;
 
-	String parentID;
+	String parentId;
 
-	public void setParentID(String id) {
-		this.parentID = id;
+	public void setParent(MultiLayeredGraph parent) {
+		MultiLayeredGraph found = null;
+		found = (MultiLayeredGraph)IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("MultiLayeredGraph"), parent.getId());
+		if( found == null ){
+			IndoorGMLMap.setFeature(parent.getId(), "MultiLayeredGraph", parent );
+		}
+		this.parentId = parent.getId();
 	}
 
-	public String getParentID() {
-		return new String(this.parentID);
+	public MultiLayeredGraph getParent() {
+		MultiLayeredGraph feature = null;
+		feature = (MultiLayeredGraph) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("MultiLayeredGraph"),
+				this.parentId);
+		return feature;
 	}
 
-	public void setStateMember(List<String> sm) {
-		this.stateMember = sm;
+	public void setStateMember(List<State> stateMember) {
+		for (int i = 0; i < stateMember.size(); i++) {
+			State found = null;
+			found = (State) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("State"),
+					stateMember.get(i).getId());
+			if (found == null) {
+				IndoorGMLMap.setFeature(stateMember.get(i).getId(), "State", stateMember.get(i));
+			}
+			if (!this.stateMember.contains(stateMember.get(i).getId())) {
+				this.stateMember.add(stateMember.get(i).getId());
+			}
+		}
+
 	}
 
-	public List<String> getStateMember() {
-		return new ArrayList<String>(this.stateMember);
+	public List<State> getStateMember() {
+		List<State> stateMember = new ArrayList<State>();
+		if (this.stateMember.size() != 0) {
+			for (int i = 0; i < this.stateMember.size(); i++) {
+				State found = (State) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("State"),
+						this.stateMember.get(i));
+				stateMember.add(found);
+			}
+		}
+		return stateMember;
 	}
 
 }
