0b545493c45e3ffc2f647604c35d2c60cbde387c
JungHam <wjdgpal7@gmail.com>
2018-01-04 17:40:00 +0900

    edit getter and setter of PrimalSpaceFeatures

--- FeatureClass/src/main/java/edu/pnu/stem/feature/PrimalSpaceFeatures.java
+++ FeatureClass/src/main/java/edu/pnu/stem/feature/PrimalSpaceFeatures.java
@@ -1,25 +1,40 @@
 package edu.pnu.stem.feature;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author jungh
- *	Implements PrimalSpaceFeaturesType of IndoorGML 1.0.3
+ * @author jungh Implements PrimalSpaceFeaturesType of IndoorGML 1.0.3
  */
-public class PrimalSpaceFeatures extends AbstractFeature{
+public class PrimalSpaceFeatures extends AbstractFeature {
 	/**
 	 * List of CellSpaces which this feature contains
 	 */
-	List<String>cellSpaceMember;
-	
+	List<String> cellSpaceMember;
+
 	/**
 	 * List of CellSpaceBoundary which this feature contains
 	 */
-	List<String>cellSpaceBoundaryMember;
+	List<String> cellSpaceBoundaryMember;
 
-	String parentID;
-	
-	public void setParentID(String id){this.parentID = id;}
-	public String getParentID(){return this.parentID;}
+	String parentId;
+
+	public void setParent(IndoorFeatures parent) {
+		IndoorFeatures found = null;
+		found = (IndoorFeatures) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("IndoorFeatures"),
+				parent.getId());
+		if (found == null) {
+			IndoorGMLMap.setFeature(parent.getId(), "IndoorFeatures", parent);
+		}
+		this.parentId = parent.getId();
+	}
+
+	public IndoorFeatures getParent() {
+		IndoorFeatures parent = null;
+		parent = (IndoorFeatures) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("IndoorFeatures"),
+				this.parentId);
+		return parent;
+	}
 
 	/**
 	 * @return the cellSpaceMember
@@ -27,22 +42,46 @@ public class PrimalSpaceFeatures extends AbstractFeature{
 	public List<String> getCellSpaceMember() {
 		return cellSpaceMember;
 	}
+
 	/**
-	 * @param cellSpaceMember the cellSpaceMember to set
+	 * @param cellSpaceMember
+	 *            the cellSpaceMember to set
 	 */
-	public void setCellSpaceMember(List<String> cellSpaceMember) {
-		this.cellSpaceMember = cellSpaceMember;
+	public void setCellSpaceMember(List<CellSpace> cellSpaceMember) {
+		for (int i = 0; i < cellSpaceMember.size(); i++) {
+			CellSpace found = null;
+			found = (CellSpace)IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("IndoorFeatures"), cellSpaceMember.get(i).getId());
+			if(found == null){
+				IndoorGMLMap.setFeature(cellSpaceMember.get(i).getId(), "IndoorFeatures" , cellSpaceMember.get(i));
+			}
+		}
 	}
+
 	/**
 	 * @return the cellSpaceBoundaryMember
 	 */
-	public List<String> getCellSpaceBoundaryMember() {
+	public List<CellSpaceBoundary> getCellSpaceBoundaryMember() {
+		List<CellSpaceBoundary>cellSpaceBoundaryMember = null;
+		if(this.cellSpaceMember != null){
+			cellSpaceBoundaryMember = new ArrayList<CellSpaceBoundary>();
+			for(int i = 0 ; i < cellSpaceBoundaryMember.size() ; i++){
+				cellSpaceBoundaryMember.add((CellSpaceBoundary)IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("CellSpaceBoundary"), this.cellSpaceMember.get(i)));
+			}
+		}
 		return cellSpaceBoundaryMember;
 	}
+
 	/**
-	 * @param cellSpaceBoundaryMember the cellSpaceBoundaryMember to set
+	 * @param cellSpaceBoundaryMember
+	 *            the cellSpaceBoundaryMember to set
 	 */
-	public void setCellSpaceBoundaryMember(List<String> cellSpaceBoundaryMember) {
-		this.cellSpaceBoundaryMember = cellSpaceBoundaryMember;
+	public void setCellSpaceBoundaryMember(List<CellSpaceBoundary> cellSpaceBoundaryMember) {
+		for (int i = 0; i < cellSpaceBoundaryMember.size(); i++) {
+			CellSpaceBoundary found = null;
+			found = (CellSpaceBoundary)IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("IndoorFeatures"), cellSpaceBoundaryMember.get(i).getId());
+			if(found == null){
+				IndoorGMLMap.setFeature(cellSpaceBoundaryMember.get(i).getId(), "CellSpaceBoundary" , cellSpaceBoundaryMember.get(i));
+			}
+		}
 	}
 }
