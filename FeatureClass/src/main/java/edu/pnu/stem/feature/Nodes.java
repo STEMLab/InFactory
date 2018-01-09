84702a7f28c1729ebff6192f04bad04aaf636cf6
JungHam <wjdgpal7@gmail.com>
2018-01-09 15:13:22 +0900

    edit constructor of Nodes for IndoorGMLMap

--- FeatureClass/src/main/java/edu/pnu/stem/feature/Nodes.java
+++ FeatureClass/src/main/java/edu/pnu/stem/feature/Nodes.java
@@ -3,47 +3,33 @@ package edu.pnu.stem.feature;
 import java.util.ArrayList;
 import java.util.List;
 
-import edu.pnu.stem.binder.Container;
-
 public class Nodes extends AbstractFeature {
 	
-	private String docId;
 	/**
 	 * State list which this feature contains
 	 */
 	private List<String> stateMember;
 
 	private String parentId;
-
+	
+	private IndoorGMLMap indoorGMLMap;
+	
+	public Nodes(IndoorGMLMap doc){
+		indoorGMLMap = doc;
+	}
+	
 	public void setParent(SpaceLayer parent) {
 		SpaceLayer found = null;
-		found = (SpaceLayer)IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("SpaceLayer"), parent.getId());
+		found = (SpaceLayer)indoorGMLMap.getFeature(indoorGMLMap.getFeatureContainer("SpaceLayer"), parent.getId());
 		if( found == null ){
-			IndoorGMLMap.setFeature(parent.getId(), "SpaceLayer", parent );
+			indoorGMLMap.setFeature(parent.getId(), "SpaceLayer", parent );
 		}
 		this.parentId = parent.getId();
 	}
 	
-	/**
-	 * @return the docId
-	 */
-	public String getDocId() {
-		return new String(this.docId);
-	}
-
-	/**
-	 * @param docId the docId to set
-	 */
-	public void setDocId(String docId) {
-		if(Container.hasDoc(docId))
-			this.docId = docId;
-		else
-			System.out.println("There is no document with that document Id.");
-	}
-	
 	public SpaceLayer getParent() {
 		SpaceLayer feature = null;
-		feature = (SpaceLayer) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("SpaceLayer"),
+		feature = (SpaceLayer) indoorGMLMap.getFeature(indoorGMLMap.getFeatureContainer("SpaceLayer"),
 				this.parentId);
 		return feature;
 	}
@@ -51,10 +37,13 @@ public class Nodes extends AbstractFeature {
 	public void setStateMember(List<State> stateMember) {
 		for (int i = 0; i < stateMember.size(); i++) {
 			State found = null;
-			found = (State) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("State"),
+			found = (State) indoorGMLMap.getFeature(indoorGMLMap.getFeatureContainer("State"),
 					stateMember.get(i).getId());
 			if (found == null) {
-				IndoorGMLMap.setFeature(stateMember.get(i).getId(), "State", stateMember.get(i));
+				indoorGMLMap.setFeature(stateMember.get(i).getId(), "State", stateMember.get(i));
+			}
+			if(this.stateMember == null){
+				this.stateMember = new ArrayList<String>();
 			}
 			if (!this.stateMember.contains(stateMember.get(i).getId())) {
 				this.stateMember.add(stateMember.get(i).getId());
@@ -67,7 +56,7 @@ public class Nodes extends AbstractFeature {
 		List<State> stateMember = new ArrayList<State>();
 		if (this.stateMember.size() != 0) {
 			for (int i = 0; i < this.stateMember.size(); i++) {
-				State found = (State) IndoorGMLMap.getFeature(IndoorGMLMap.getFeatureContainer("State"),
+				State found = (State) indoorGMLMap.getFeature(indoorGMLMap.getFeatureContainer("State"),
 						this.stateMember.get(i));
 				stateMember.add(found);
 			}
