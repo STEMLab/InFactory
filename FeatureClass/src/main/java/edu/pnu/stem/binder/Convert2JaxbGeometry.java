dec064ce1e23d4bddedf46dc345503395658d64a
JungHam <wjdgpal7@gmail.com>
2018-01-15 21:11:21 +0900

    change function to static type at Convert2jaxbGeometry

--- FeatureClass/src/main/java/edu/pnu/stem/binder/Convert2JaxbGeometry.java
+++ FeatureClass/src/main/java/edu/pnu/stem/binder/Convert2JaxbGeometry.java
@@ -16,23 +16,22 @@ import net.opengis.gml.v_3_2_1.DirectPositionListType;
 import net.opengis.gml.v_3_2_1.DirectPositionType;
 import net.opengis.gml.v_3_2_1.LineStringType;
 import net.opengis.gml.v_3_2_1.LinearRingType;
-import net.opengis.gml.v_3_2_1.ObjectFactory;
 import net.opengis.gml.v_3_2_1.PointType;
 import net.opengis.gml.v_3_2_1.ShellType;
 import net.opengis.gml.v_3_2_1.SolidType;
 import net.opengis.gml.v_3_2_1.SurfaceType;
 
 public class Convert2JaxbGeometry {
-	ObjectFactory gmlFactory;
+	private final static net.opengis.gml.v_3_2_1.ObjectFactory gmlFactory = new net.opengis.gml.v_3_2_1.ObjectFactory();
 	
-	public SolidType Convert2SolidType(Solid feature){
+	public static SolidType Convert2SolidType(Solid feature){
 		MultiPolygon shell = feature.getShell();
 		SolidType newFeature = gmlFactory.createSolidType();
 		Convert2ShellType(shell);
 		return newFeature;
 	}
 	
-	public ShellType Convert2ShellType(MultiPolygon feature){
+	public static ShellType Convert2ShellType(MultiPolygon feature){
 		List<Polygon>polygonList = new ArrayList<Polygon>();
 		ShellType newFeature = gmlFactory.createShellType();
 		for(int i = 0 ; i < feature.getLength() ; i++){
@@ -41,13 +40,13 @@ public class Convert2JaxbGeometry {
 		return newFeature;
 	}
 	
-	public SurfaceType Convert2SurfaceType(Polygon feature){
+	public static SurfaceType Convert2SurfaceType(Polygon feature){
 		Convert2LinearRingType((LinearRing)feature.getExteriorRing());
 		SurfaceType newFeature = gmlFactory.createSurfaceType();
 		return newFeature;
 	}
 	
-	public LinearRingType Convert2LinearRingType(LinearRing feature){
+	public static LinearRingType Convert2LinearRingType(LinearRing feature){
 		List<Point>pointList = new ArrayList<Point>();
 		LinearRingType newFeature = gmlFactory.createLinearRingType();
 		for(int i = 0 ; i < feature.getLength() ; i++){
@@ -56,7 +55,7 @@ public class Convert2JaxbGeometry {
 		return newFeature;
 	}
 	
-	public LineStringType Convert2LineStringType(LineString feature){
+	public static LineStringType Convert2LineStringType(LineString feature){
 		LineStringType newFeature = gmlFactory.createLineStringType();
 		DirectPositionListType directPost = gmlFactory.createDirectPositionListType();
 		List<Coordinate>coordList = new ArrayList<Coordinate>();
@@ -67,7 +66,7 @@ public class Convert2JaxbGeometry {
 		return newFeature;
 	}
 	
-	public PointType Convert2PointType(Point feature){
+	public static PointType Convert2PointType(Point feature){
 		PointType newFeature = gmlFactory.createPointType();
 		newFeature.setPos(Convert2CoordinateType(feature.getCoordinateSequence().getCoordinate(0), feature.getDimension()));
 		return newFeature;
@@ -77,7 +76,7 @@ public class Convert2JaxbGeometry {
 		DirectPositionListType newFeature = gmlFactory.createDirectPositionListType();
 		return newFeature;
 	}
-	public DirectPositionType Convert2CoordinateType(Coordinate feature, int dimension){
+	public static DirectPositionType Convert2CoordinateType(Coordinate feature, int dimension){
 		DirectPositionType newFeature = gmlFactory.createDirectPositionType();
 		if(dimension == 2){
 			List<Double> points = new ArrayList<Double>();
