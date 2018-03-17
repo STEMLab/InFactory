24a90d2ee6d2615a1f39c24688653a024caeebda
JungHam <wjdgpal7@gmail.com>
2018-03-17 19:42:24 +0900

    create make~ functions and implemented them

--- igml-feature/src/main/java/edu/pnu/stem/binder/Convert2GeoJsonGeometry.java
+++ igml-feature/src/main/java/edu/pnu/stem/binder/Convert2GeoJsonGeometry.java
@@ -1,20 +1,23 @@
 package edu.pnu.stem.binder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class Convert2GeoJsonGeometry {
 
-	public JsonNode convert2GeoJson(JsonNode geometry){
+	public JsonNode convert2GeoJson(JsonNode geometry, String featureType){
 		JsonNode result = null;
 		String geometryType = geometry.get("type").asText().trim();
 		if(geometryType.equals("Solid")){
 			result = convert2Solid3D(geometry);
 		}
 		else if(geometryType.equals("Surface")){
-			if(geometry.get("propertie").get("extrude").asText().trim().equals("true")){
+			if(geometry.get("properties").get("extrude").asText().trim().equals("true")){
 				result = convert2Surface3D(geometry);
 			}
-			else if(geometry.get("propertie").get("extrude").asText().trim().equals("false")){
+			else if(geometry.get("properties").get("extrude").asText().trim().equals("false")){
 				result = convert2Surface2D(geometry);
 			}
 			else{
@@ -22,7 +25,18 @@ public class Convert2GeoJsonGeometry {
 			}
 		}
 		else if(geometryType.equals("Curve")){
-			result = convert2Curve2D(geometry);
+			if(featureType.equals("CellSpaceBoundary")){
+				if(geometry.get("properties").get("extrude").asText().trim().equals("true")){
+					result = convert2ExtrudedCurve3D(geometry);
+				}
+				else if(geometry.get("properties").get("extrude").asText().trim().equals("false")){
+					result = convert2Curve2D(geometry);
+				}
+			}
+			else if(featureType.equals("Transition")){
+				result = convert2Curve2D(geometry);
+			}
+			
 		}
 		else if(geometryType.equals("Point")){
 			result = convert2Point2D(geometry);
@@ -35,6 +49,8 @@ public class Convert2GeoJsonGeometry {
 	}
 	public JsonNode convert2Solid3D(JsonNode geometry){
 		JsonNode result = null;
+		JsonNode coordinates = geometry.get("coordinates");
+		
 		return result;
 	}
 	public JsonNode convert2Surface3D(JsonNode geometry){
@@ -53,5 +69,110 @@ public class Convert2GeoJsonGeometry {
 		JsonNode result = null;
 		return result;
 	}
+	public JsonNode convert2ExtrudedCurve3D(JsonNode geometry){
+		JsonNode result = null;
+		return result;
+		
+	}
+	public List<List> makeSolid(List<List> coordinates, Double height){
+		
+		List<List> result = new ArrayList<List>();
+		List<List> exterior = new ArrayList<List>();
+		List<List> interior = new ArrayList<List>();
+
+		//make 3d surfaces of the side
+		for(int i = 0 ; i < coordinates.size(); i++){
+			List<List> surface = new ArrayList<List>();
+			List<Double> extrudedPoint = new ArrayList<Double>();
+			Double ground = 0.0;
+			surface.add(coordinates.get(i));
+			surface.add(coordinates.get(i+1));
+			
+			extrudedPoint = coordinates.get(i+1);
+			ground = extrudedPoint.get(2);
+			ground += height;
+			extrudedPoint.remove(2);
+			extrudedPoint.add(ground);
+			
+			surface.add(extrudedPoint);
+			
+			extrudedPoint = coordinates.get(i);
+			ground = extrudedPoint.get(2);
+			ground += height;
+			extrudedPoint.remove(2);
+			extrudedPoint.add(height);
+			
+			surface.add(extrudedPoint);
+			
+			exterior.add(surface);
+		}
+		
+		//make 3d surface of the lid
+		
+		List<List> upperSurface = new ArrayList<List>();
+		List<List> lowerSurface = new ArrayList<List>();
+		
+		for(int i = coordinates.size()-1 ; i !=0; i--){
+			lowerSurface.add(coordinates.get(i));
+		}
+		
+		for(int i = 0 ; i < coordinates.size(); i++){
+			List<Double>extrudedPoint = coordinates.get(i);
+			Double ground = 0.0;
+			
+			ground = extrudedPoint.get(2);
+			ground += height;
+			extrudedPoint.remove(2);
+			extrudedPoint.add(2, ground);
+			upperSurface.add(extrudedPoint);
+		}
+		exterior.add(upperSurface);
+		exterior.add(lowerSurface);
+		
+		result.add(exterior);
+		result.add(interior);
+		
+		return result;
+	}
+	public List<List> makeSurface3D(List<List> coordinates, Double height){
+		//List<List> result = new ArrayList<List>();
+		
+		for(List coord : coordinates){
+			Double ground = .0;
+			
+			ground = (Double) coord.get(2);
+			ground += height;
+			coord.remove(2);
+			coord.add(ground);
+			
+		}
+		
+		//result.add(coordinates);
+		
+		return coordinates;
+		
+	}
+	
+	public List<List> makeExtrudedCurve(List<List> coordinates, Double height){
+		List<List>result = new ArrayList<List>();
+		//TODO : need to support linesting which has several points
+		List<Double> upperRight = new ArrayList<Double>();
+		List<Double> upperLeft = new ArrayList<Double>();
+		
+		upperRight.add((Double)coordinates.get(1).get(0));
+		upperRight.add((Double)coordinates.get(1).get(1));
+		upperRight.add((Double)coordinates.get(1).get(2)+height);
+		
+		upperLeft.add((Double)coordinates.get(0).get(0));
+		upperLeft.add((Double)coordinates.get(0).get(1));
+		upperLeft.add((Double)coordinates.get(0).get(2)+height);
+		
+		coordinates.add(upperRight);
+		coordinates.add(upperLeft);
+ 		
+		//result.add(coordinates);
+		return coordinates;
+	}
+	
 	
 }
