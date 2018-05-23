3260ad6703ad2797bef62293bc94fc5532bc65d8
Hyemi Jeong <wjdgpal7@gmail.com>
2018-05-23 17:27:47 +0900

    create readCellSpace

--- igml-feature/src/main/java/edu/pnu/stem/dao/CellSpaceDAO.java
+++ igml-feature/src/main/java/edu/pnu/stem/dao/CellSpaceDAO.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.io.ParseException;
@@ -15,6 +17,8 @@ import edu.pnu.stem.feature.PrimalSpaceFeatures;
 import edu.pnu.stem.feature.State;
 import edu.pnu.stem.geometry.jts.Solid;
 import edu.pnu.stem.geometry.jts.WKTReader3D;
+import edu.pnu.stem.geometry.jts.WKTWriter3D;
+import edu.pnu.stem.util.GeometryUtil;
 
 /**
  * @author jungh
@@ -61,6 +65,69 @@ public class CellSpaceDAO {
 		return newFeature;
 	}
 	*/
+	public static ObjectNode readCellSpace(IndoorGMLMap map, String id) {
+		ObjectNode result = JsonNodeFactory.instance.objectNode();
+		CellSpace target = (CellSpace)map.getFeature(id);
+		WKTWriter3D writer = new WKTWriter3D();
+		ObjectNode properties = null;
+		result.put("type","CellSpace");
+		Geometry targetGeometry = target.getGeometry();
+		
+		if(targetGeometry != null) {
+			ObjectNode geometryData = JsonNodeFactory.instance.objectNode();
+			ObjectNode geometryProperties = JsonNodeFactory.instance.objectNode();
+			
+			
+			geometryProperties.put("id", GeometryUtil.getMetadata(targetGeometry, "id"));
+			geometryProperties.put("type","wkt");
+			
+			if(targetGeometry instanceof Solid) {			
+				geometryData.put("type", "Solid");
+				
+			}
+			else if(targetGeometry instanceof Polygon) {
+				geometryData.put("type", "Polygon");
+			}
+			else {
+				//error
+			}
+			String wktGeometry = writer.write(targetGeometry);
+			geometryData.put("coordinate",wktGeometry);
+			geometryData.set("properties", geometryProperties);
+			result.set("geometry", geometryData);
+			//geometryData.put(fieldName, targetGeometry);
+		}
+		
+		result.put("parentId",target.getParent().getId());
+		result.put("id",id);
+		result.put("docId", map.getDocId());
+		
+		if(target.getDuality() != null) {
+			properties = JsonNodeFactory.instance.objectNode();
+			properties.put("duality", target.getDuality().getId());
+			
+		}
+		
+		if(target.getName() != null) {
+			if(properties == null) {
+				properties = JsonNodeFactory.instance.objectNode();
+			}
+			properties.put("name", target.getName());
+		}
+		
+		if(target.getDescription() != null) {
+			if(properties == null) {
+				properties = JsonNodeFactory.instance.objectNode();
+			}
+			properties.put("description", target.getDescription());
+		}
+		
+		if(properties != null) {
+			result.set("properties", properties);
+		}
+		
+		return result;
+	}
 	public static CellSpace createCellSpace(IndoorGMLMap map, String parentId, String id, JsonNode geometry, String duality, List<String> partialBoundedBy) {
 		if(id == null) {
 			id = UUID.randomUUID().toString();
