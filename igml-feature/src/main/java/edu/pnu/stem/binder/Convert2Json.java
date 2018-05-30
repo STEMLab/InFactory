530d84eaeccc3ab22c7c697ad28aa3e0cf3057a2
Hyemi Jeong <wjdgpal7@gmail.com>
2018-05-30 15:18:59 +0900

    add geometry convertion to Json

--- igml-feature/src/main/java/edu/pnu/stem/binder/Convert2Json.java
+++ igml-feature/src/main/java/edu/pnu/stem/binder/Convert2Json.java
@@ -1,17 +1,22 @@
 package edu.pnu.stem.binder;
 
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.ParseException;
 
 import edu.pnu.stem.feature.CellSpace;
 import edu.pnu.stem.geometry.jts.Solid;
+import edu.pnu.stem.geometry.jts.WKTReader3D;
 import edu.pnu.stem.geometry.jts.WKTWriter3D;
 import edu.pnu.stem.util.GeometryUtil;
 
 public class Convert2Json {
-	public ObjectNode convert2CellSpaceJSON(IndoorGMLMap map, CellSpace target) {
+	public static ObjectNode convert2CellSpaceJSON(IndoorGMLMap map, CellSpace target) {
 		ObjectNode result = JsonNodeFactory.instance.objectNode();
 		WKTWriter3D writer = new WKTWriter3D();
 		ObjectNode properties = null;
@@ -73,4 +78,58 @@ public class Convert2Json {
 		
 		return result;
 	} 
+	public static Geometry json2Geometry(JsonNode geometry) {
+		Geometry result = null;
+			/*
+			 * String geometryType = geometry.get("properties").get("type").asText().trim();				
+				JsonNode solid = Convert2GeoJsonGeometry.convert2GeoJson(geometry, geometryType);
+				GeoJSON3DReader reader = new GeoJSON3DReader();
+				Solid resultSolid = (Solid)reader.read(solid.toString());
+				map.setFeature4Geometry(geometry.get("properties").get("id").asText().trim(), resultSolid);
+				newFeature.setGeometry(resultSolid);
+			 * */
+			WKTReader3D wkt = new WKTReader3D();
+			Geometry wktG = null;
+			if(geometry.has("coordinates")){			
+				try {					
+					wktG = wkt.read(geometry.get("coordinates").asText().trim());
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else{
+				//WKT directly
+				try{
+					wktG = wkt.read(geometry.asText().trim());
+				}catch (ParseException e) {
+					e.printStackTrace();
+				}
+			}
+			String geometryId = null;
+			if(geometry.has("properties")){
+				if(geometry.get("properties").has("id")){
+					geometryId = geometry.get("properties").get("id").asText().trim();
+				}
+			}
+			if(geometryId == null){
+				geometryId = UUID.randomUUID().toString();
+			}
+			if(wktG != null){
+				if(wktG instanceof Solid){
+					result = (Solid)wktG;
+				}
+				else if(wktG instanceof Polygon){
+					result = (Polygon)wktG;
+					
+				}
+				else{
+					//TODO : Exception
+				}
+			}
+						
+		
+		
+		return result;
+	}
 }
