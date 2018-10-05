d6c1a03d8ce53b7c06d39ff1d75a81cab464fb2d
JungHam <wjdgpal7@gmail.com>
2018-10-05 18:39:04 +0900

    create createfunction at controller

--- igml-feature/src/main/java/edu/pnu/stem/dao/InterLayerConnectionDAO.java
+++ igml-feature/src/main/java/edu/pnu/stem/dao/InterLayerConnectionDAO.java
@@ -10,7 +10,7 @@ import edu.pnu.stem.feature.SpaceLayer;
 import edu.pnu.stem.feature.State;
 public class InterLayerConnectionDAO {
 
-	public static InterLayerConnection createInterLayerConnection(IndoorGMLMap map, String parentId, String id, String name, String description, String typeOfTopoExpression, String comment, String[] interConnects, String[] ConnectedLayers){
+	public static InterLayerConnection createInterLayerConnection(IndoorGMLMap map, String parentId, String id, String name, String description, String typeOfTopoExpression, String comment, String[] interConnects, String[] connectedLayers){
 		InterLayerConnection newFeature = new InterLayerConnection(map, id);
 		InterEdges parent = new InterEdges(map, parentId);
 		newFeature.setParent(parent);
@@ -54,33 +54,23 @@ public class InterLayerConnectionDAO {
 		if(description != null) {
 			newFeature.setDescription(description);
 		}
-		if(interConnects.length == 2 && ConnectedLayers.length == 2){
-			if(map.hasID(interConnects[0])&&map.hasID(interConnects[1])){
-				if(map.hasID(ConnectedLayers[0])&&map.hasID(ConnectedLayers[0])){
-					State[] tempInterLayerConnectionList = new State[2];
-					tempInterLayerConnectionList[0] = new State(map, interConnects[0]);
-					tempInterLayerConnectionList[1] = new State(map, interConnects[1]);
-					newFeature.setInterConnects(tempInterLayerConnectionList);
-					
-					SpaceLayer[] tempConnectedLayers = new SpaceLayer[2];
-					tempConnectedLayers[0] = new SpaceLayer(map, ConnectedLayers[0]);
-					tempConnectedLayers[1] = new SpaceLayer(map, ConnectedLayers[1]);
-					newFeature.setConnectedLayers(tempConnectedLayers);
-				}
-				else{
-					System.out.println("Error at createInterLayerConnection : This SpaceLayer is not exist");
-				}
-			}
-			else{
-				System.out.println("Error at createInterLayerConnection : This State is not exist");
-			}
+		if(interConnects.length == 2 && connectedLayers.length == 2){
+			State[] tempInterLayerConnectionList = new State[2];
+			tempInterLayerConnectionList[0] = new State(map, interConnects[0]);
+			tempInterLayerConnectionList[1] = new State(map, interConnects[1]);
+			newFeature.setInterConnects(tempInterLayerConnectionList);
+			
+			SpaceLayer[] tempConnectedLayers = new SpaceLayer[2];
+			tempConnectedLayers[0] = new SpaceLayer(map, connectedLayers[0]);
+			tempConnectedLayers[1] = new SpaceLayer(map, connectedLayers[1]);
+			newFeature.setConnectedLayers(tempConnectedLayers);
 		}
 		else{
 			System.out.println("Error at createInterLayerConnection : There is no enough instance of interConnects or ConnectedLayers");
 		}
 		
 		map.removeFutureID(id);
-		map.setFeature(id, "interLayerConnection", newFeature);
+		map.setFeature(id, "InterLayerConnection", newFeature);
 		
 		return newFeature;
 		
