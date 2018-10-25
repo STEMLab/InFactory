e50976d13fc08508d13503148f41396d499be75d
JungHam <wjdgpal7@gmail.com>
2018-10-25 14:52:00 +0900

    create navigation module feature classes
    InFactory firstly will support the part of IndoorGML Navigation Module

---
+++ igml-feature/src/main/java/edu/pnu/stem/feature/navigation/NonNavigableBoundary.java
@@ -0,0 +1,13 @@
+package edu.pnu.stem.feature.navigation;
+
+import edu.pnu.stem.binder.IndoorGMLMap;
+import edu.pnu.stem.feature.core.CellSpaceBoundary;
+
+public class NonNavigableBoundary extends CellSpaceBoundary{
+
+	public NonNavigableBoundary(IndoorGMLMap doc, String id) {
+		super(doc, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ igml-feature/src/main/java/edu/pnu/stem/feature/navigation/NonNavigableSpace.java
@@ -0,0 +1,13 @@
+package edu.pnu.stem.feature.navigation;
+
+import edu.pnu.stem.binder.IndoorGMLMap;
+import edu.pnu.stem.feature.core.CellSpace;
+
+public class NonNavigableSpace extends CellSpace{
+
+	public NonNavigableSpace(IndoorGMLMap doc, String id) {
+		super(doc, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
